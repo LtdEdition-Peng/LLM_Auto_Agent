6da7b2016e9e80873195f2ae5081b281c8387884
LtdEdition-Peng <163836488+LtdEdition-Peng@users.noreply.github.com>
2025-07-31 19:28:07 +0800

    change name

--- run_agent.py
+++ run_agent.py
@@ -7,7 +7,7 @@ def main():
     config.max_steps = 10
     config.refresh_prompt_interval = 30
     config.show_system_messages = False  # 显示中间信息，如果不需要可以设置为False
-    config.conda = "new"
+    config.conda = "New"  # 请提前安装anaconda，并且创建名字为New环境，如果有其他环境，请修改agenttools里面的调用python的默认环境（在此处修改无效，这是一个bug）
 
     # 创建并运行Agent
     agent = ReactAgent(config)
